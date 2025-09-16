f68daf53fc2953ca3497a8c3358d97459067b32c
lucas <lucas46em@gmail.com>
2025-09-16 17:49:44 -0300

    inittial commit for project 7

---
+++ 7_document_classifier/frontend/components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Hello World 🚀</h1>
+      <p>Esse é o primeiro componente do projeto.</p>
+    </div>
+  );
+}
