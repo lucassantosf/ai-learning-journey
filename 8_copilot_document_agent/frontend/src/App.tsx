0d632bf353d41d5b8e62f4bcab3d3eeb0e12e751
lucas <lucas46em@gmail.com>
2025-10-16 18:43:42 -0300

    initial commit project 8

---
+++ 8_copilot_document_agent/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Copilot Document Agent</h1>
+        <p>Welcome to your project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
